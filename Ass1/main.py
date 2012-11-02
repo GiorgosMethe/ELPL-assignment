a2f1c9e9dbda0ad36bc217860886bd529cbeeadf
Giorgos Methenitis <girorgosmethe@gmail.com>
2012-11-02 01:47:04 +0100

    almost ready!

--- Ass1/main.py
+++ Ass1/main.py
@@ -1,6 +1,6 @@
-#(TOP (S (NP (NNP Ms.) (NNP Haag)) (S@ (VP (VBZ plays) (NP (NNP Elianti))) (. .))) )
 import tree_parser
 
 f = open('trainingSet.txt',"r")
 text = f.read()
 tree_parser.parseDocument(text,0,-1)
+tree_parser.printRules()
--- Ass1/tree_parser.py
+++ Ass1/tree_parser.py
@@ -1,15 +1,41 @@
 # parser module
-
+from pprint import pprint
 tempArray = []
+
+ruleDict = dict()
 
 class ParsedRules(object):
     def __init__(self, rule, lvl):
 		self.rule = rule
 		self.lvl = lvl
 
+class Rule(object):
+    def __init__(self, leftSide, rightSide):
+		self.leftSide = leftSide
+		self.rightSide = rightSide
+
 def createRules():
-	for j in range(len(tempArray)):
-		print tempArray[j].rule, tempArray[j].lvl
+	for i in range(len(tempArray)-1):
+		rightSide = ""
+		for j in range(i+1,len(tempArray)):
+			if(tempArray[j].lvl-1 == tempArray[i].lvl):
+				if(rightSide == ""):
+					rightSide += tempArray[j].rule
+				else:
+					rightSide += " " + tempArray[j].rule
+			if(j == len(tempArray)-1 or tempArray[j].lvl == tempArray[i].lvl):
+				if rightSide != "" :
+					saveRule(Rule(tempArray[i].rule,rightSide))
+				break
+
+def saveRule(newRule):
+	if newRule.leftSide+" "+newRule.rightSide in ruleDict:
+		ruleDict[newRule.leftSide+" "+newRule.rightSide] += 1
+	else:
+		ruleDict[newRule.leftSide+" "+newRule.rightSide] = 1
+
+def printRules():
+	pprint(ruleDict)
 
 def parseDocument(inputText,start,lvl):
 	openP = 0
@@ -32,12 +58,15 @@ def parseDocument(inputText,start,lvl):
 			parseDocument(inputText, i+1, lvl)
 		else:
 			a += inputText[i]
-		if (openP == closeP):		
-			if (inputText[i+1] == "\n"):
-				lvl = -1
-				parseDocument(inputText, i+2, lvl)
+		if (openP == closeP):
+			if (i+1 > len(inputText)-1):
 				createRules()
-			break
+			else:
+				if inputText[i+1] == "\n":
+					lvl = -1
+					parseDocument(inputText, i+2, lvl)
+					tempArray[:] = []
+				break
 	
 def fixRules(inputString,level):
 	fixed = ""
