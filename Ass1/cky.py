b40f955e8f0624637c3a53a7e4abc88fefd22545
GiorgosMethe <giorgosmethe@gmail.com>
2012-11-13 23:53:20 +0100

    nothing serious

--- Ass1/cky.py
+++ Ass1/cky.py
@@ -29,22 +29,6 @@ def itarateSentences():
 	for s in sentences:
 		ckyAlgorithm(s)
 
-# let the input be a string S consisting of n characters: a1 ... an.
-# let the grammar contain r nonterminal symbols R1 ... Rr.
-# This grammar contains the subset Rs which is the set of start symbols.
-# let P[n,n,r] be an array of booleans. Initialize all elements of P to false.
-# for each i = 1 to n
-#   for each unit production Rj -> ai
-#     set P[i,1,j] = true
-# for each i = 2 to n -- Length of span
-#   for each j = 1 to n-i+1 -- Start of span
-#     for each k = 1 to i-1 -- Partition of span
-#       for each production RA -> RB RC
-#         if P[j,k,B] and P[j+k,i-k,C] then set P[j,i,A] = true
-# if any of P[1,n,x] is true (x is iterated over the set s, where s are all the indices for Rs) then
-#   S is member of language
-# else
-#   S is not member of language
 
 def ckyAlgorithm(s):
 	CKYtable = initialize_threedlist(len(s))
@@ -74,7 +58,6 @@ def ckyAlgorithm(s):
 
 	# printCKYtable(CKYtable);
 
-	
 
 def printCKYtable(CKYtable):
 	pass
