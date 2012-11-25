bde736b1428673c137bfb380761e5797ddaee67f
GiorgosMethe <giorgosmethe@gmail.com>
2012-11-25 01:11:35 +0100

    utilities added to the code to find ambiguous words report almost completed

--- Ass1/run.py
+++ Ass1/run.py
@@ -2,16 +2,21 @@ import tree_parser
 import cky
 import sys
 import argparse
+import utilities
 
 help = {
 	'e':	'Generate a grammar from the passed treebank.',
 	'ps':	'CYK parser, takes a sentence as argument.',
 	'pf':	'CYK parser, takes a .',
-	's':	'Show grammar'
+	's':	'Show grammar',
+	'm':	'Most likely productions',
+	'a':  'Ambiguous words'
 }
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--treebank', nargs=2, help=help['e'])
-parser.add_argument('-s', '--s_grammar_file', help=help['s'])
+parser.add_argument('-s', '--s_grammar_file' , help=help['s'])
+parser.add_argument('-a', '--a_grammar_file' , help=help['s'])
+parser.add_argument('-m', '--m_grammar_file',nargs = 2, help=help['m'])
 parser.add_argument('-ps', '--ps_grammar_file', nargs=2 , help=help['ps'])
 parser.add_argument('-pf', '--pf_grammar_file', nargs=2 , help=help['pf'])
 args = parser.parse_args()
@@ -51,3 +56,11 @@ if args.treebank:
 	tree_parser.parse_document(text,0,-1)
 	tree_parser.fix_probs()
 	tree_parser.write_file(args.treebank[1])
+
+if args.m_grammar_file:
+	utilities.read_pcfg(args.m_grammar_file[0])
+	utilities.most_likely_production(args.m_grammar_file[1],4)
+
+if args.a_grammar_file:
+	utilities.read_pcfg(args.a_grammar_file)
+	utilities.ambiguous()
---
+++ Ass1/utilities.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+from operator import itemgetter, attrgetter
+import collections
+
+""" Dictionary to hold the rules from the extracted pcfg
+Binary rules are held like:
+<<rulesRL[right_side][left_side] = prob>>
+"""
+rulesRL = defaultdict(set)
+rulesLR = defaultdict(set)
+
+class item:
+	def __init__(self, rule, prob):
+		self.rule = rule
+		self.prob = prob
+
+class ambiguous_item:
+	def __init__(self, ruleL, ruleR, num, prob):
+		self.ruleL = ruleL
+		self.ruleR = ruleR
+		self.num = num
+		self.prob = prob
+
+def read_pcfg(file_name):
+	"""Reads the grammar rules from the output of the pcfg
+	parser.
+	Output: writes the rulesRL dictionary
+	"""
+	for line in open(file_name, "r"):
+		values = line.split(" ")
+		if len(values) == 6:
+			if type(rulesRL[(values[2],values[3])])!=type({}):
+				rulesRL[(values[2],values[3])] = dict()
+			rulesRL[(values[2],values[3])][values[1]] = values[4]
+			if type(rulesLR[values[1]])!=type({}):
+				rulesLR[values[1]] = dict()
+			rulesLR[values[1]][(values[2],values[3])] = values[4] 
+		else:
+			if type(rulesRL[values[2]])!=type({}):
+				rulesRL[values[2]] = dict()
+			rulesRL[values[2]][values[1]] = values[3]
+			if type(rulesLR[values[1]])!=type({}):
+				rulesLR[values[1]] = dict()
+			rulesLR[values[1]][values[2]] = values[3]
+
+def most_likely_production(noTerminal,num):
+	most_likely = []
+	for key,value in rulesLR[noTerminal].iteritems():
+		most_likely.append(item(key,float(value)))
+		if len(most_likely) > num:
+			most_likely = sorted(most_likely, key=attrgetter('prob'))
+			most_likely.pop(0)
+	for n in  sorted(most_likely, key=attrgetter('prob'), reverse=True):
+		print n.rule, n.prob
+
+def ambiguous():
+	ambiguous_words = []
+	for key,value in rulesRL.iteritems():
+		if isinstance(key, basestring) and key not in rulesLR:
+			counter = 0
+			for key1,value1 in rulesRL[key].iteritems():
+				counter +=1
+			for key1,value1 in rulesRL[key].iteritems():
+				ambiguous_words.append(ambiguous_item(key,key1,counter,value1))
+
+	for n in  sorted(ambiguous_words, key=attrgetter('num'), reverse=True):
+		if n.num > 4:
+			print n.ruleL," & ",n.ruleR," & ", "%0.6f" % float(n.prob)," \\\\"
